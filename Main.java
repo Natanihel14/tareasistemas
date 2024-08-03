b0ede84e28fda102522c0fa1733edc857d95e4f4
Natanihel14 <nataruano14@gmail.com>
2024-08-02 23:46:42 -0600

    Version 3

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Main {
@@ -5,6 +6,16 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingresa tu nombre: ");
         String nombre = scanner.nextLine();
-        System.out.println("Hola, " + nombre);
+
+        LocalTime now = LocalTime.now();
+        int hour = now.getHour();
+
+        if (hour < 12) {
+            System.out.println("Buenos días, " + nombre);
+        } else if (hour < 18) {
+            System.out.println("Buenas tardes, " + nombre);
+        } else {
+            System.out.println("Buenas noches, " + nombre);
+        }
     }
 }
